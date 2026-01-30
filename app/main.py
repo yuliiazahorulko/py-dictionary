7b4d85fcb9b8ee268b0dc838d33ac3eb48c6b50b
Yuliia Zahoruulko <yzahorulko@gmail.com>
2026-01-30 14:39:07 +0200

    fixed comments but with bugs

--- app/main.py
+++ app/main.py
@@ -9,13 +9,12 @@ class Dictionary:
         load_factor_index = hash(key) % self.capacity
         if 0 <= load_factor_index < self.capacity:
             index = load_factor_index
-        else:
-            raise KeyError
         if self.size <= self.threshold and self.dictionary[index] is None:
-            self.dictionary[index] = [key, value]
+            self.dictionary[index] = [key, hash(key), value]
+            self.size += 1
         elif self.dictionary[index] is not None:
-            if self.dictionary[index][0] == key:
-                self.dictionary[index][1] = value
+            if self.dictionary[index][1] == hash(key):
+                self.dictionary[index][2] = value
             else:
                 while True:
                     temp_list = list(range(index + 1, len(self.dictionary))) \
@@ -23,29 +22,39 @@ class Dictionary:
                     for i in temp_list:
                         if self.dictionary[i] is not None:
                             continue
-                        self.dictionary[i] = [key, value]
+                        self.dictionary[i] = [key, hash(key), value]
+                        self.size += 1
                         break
                     break
 
     def __setitem__(self, key: str | int, value: str | int) -> None:
-        self.size += 1
         if self.size <= self.threshold:
             self.calculation_algorithm(key, value)
         elif self.size > self.threshold:
             self.dictionary += [None] * self.capacity
+            temp = []
+            for item in self.dictionary:
+                if item is not None:
+                    temp.append(item)
+
             self.capacity = self.capacity * 2
+            self.dictionary = [None] * self.capacity
             self.threshold = 2 * self.capacity // 3
+
+            for temp_item in temp:
+                self.calculation_algorithm(temp_item[0], temp_item[2])
+
             self.calculation_algorithm(key, value)
 
     def __getitem__(self, item: str | int) -> None:
         try:
             if item in self.dictionary:
-                return self.dictionary[item]
+                return self.dictionary[item][1]
         except KeyError:
-            raise KeyError
+            raise KeyError("Key outside the dictionary!")
 
     def __len__(self) -> int:
-        return len(self.dictionary)
+        return self.size
 
     def __repr__(self) -> str:
         return f"{{{[key_value for key_value in self.dictionary]}}}"
